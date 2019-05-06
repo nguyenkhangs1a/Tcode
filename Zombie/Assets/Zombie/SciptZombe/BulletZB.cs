57f652ee7c579620e2b0d2307d1e96109a8bdd61
nguyenkhangs1a <nguyenkhangs1a@gmail.com>
2019-05-06 13:11:21 +0700

    Zombie  Bug

---
+++ Zombie/Assets/Zombie/SciptZombe/BulletZB.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletZB : MonoBehaviour {
+    [SerializeField]
+    private int dame;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Getdame");
+            col.transform.gameObject.GetComponent<MovePlayer>().Gethit(dame);
+            Destroy(gameObject);
+        }
+    }
+}
